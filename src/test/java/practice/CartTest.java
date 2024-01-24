6488fcbe5a821ad752122ca23eaa539ad5f76c81
nash <nash@DESKTOP-GLAFMTS>
2024-01-24 17:44:40 +0530

    cart page is ready

---
+++ src/test/java/practice/CartTest.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class CartTest {
+	
+	@Test
+	public void myCartTest()
+	{
+		System.out.println(" my cart page ready ");
+	}
+
+}
