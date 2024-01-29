924e2fa5b029390cd273ea59aec39fdb9bf7fff0
PriyankaML <priyankamlmys@gmail.com>
2024-01-29 13:18:00 +0530

    start jenkins

---
+++ src/test/java/practice/carryCartToBranchTest.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class carryCartToBranchTest {
+
+	@Test
+	public void cartTo()
+	{
+		System.out.println("cart to added");
+		
+		System.out.println("not yet done");
+	}
+}
