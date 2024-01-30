18d753d904043e88a4ae57c36ee78de01a877b61
PriyankaML <priyankamlmys@gmail.com>
2024-01-30 17:19:54 +0530

    committed

---
+++ src/test/java/practice/jenkinsPipelineTest.java
@@ -0,0 +1,32 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class jenkinsPipelineTest {
+
+	
+	@Test
+	public void pipelineTest()
+	{
+		String BROWSER = System.getProperty("browser");
+		String ENV = System.getProperty("env");
+		
+		System.out.println(BROWSER);
+		System.out.println(ENV);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
